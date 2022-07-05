6355fbfb0968cc4854028344e0c0751bf7e3781c
Eity2021 <Eity2021akter@gmail.com>
2022-07-05 14:03:03 +0600

    google authentication

--- src/Component/LogIn/LogIn.js
+++ src/Component/LogIn/LogIn.js
@@ -1,70 +1,70 @@
-// import React from "react";
-// import { Button, Form } from "react-bootstrap";
-// import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
-// import { Link } from "react-router-dom";
-// import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-// import { getAuth } from "firebase/auth";
-// import app from "../../firebase.init";
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
+import app from "../../firebase.init";
 
-// const auth = getAuth(app);
-// const LogIn = () => {
-//   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-//   return (
-//     <div>
-//       <div className="main_registered_part">
-//         <div className="register_area">
-//           <h1 className="reg_heading"> Sign Up </h1>
+const auth = getAuth(app);
+const LogIn = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  return (
+    <div>
+      <div className="main_registered_part">
+        <div className="register_area">
+          <h1 className="reg_heading"> Sign Up </h1>
 
-//           <div className="register">
-//             <Form>
-//               <Form.Group className="mb-3 margin" controlId="formBasicEmail">
-//                 <Form.Control
-//                   className="form-input"
-//                   type="email"
-//                   placeholder="Enter email"
-//                 />
-//               </Form.Group>
+          <div className="register">
+            <Form>
+              <Form.Group className="mb-3 margin" controlId="formBasicEmail">
+                <Form.Control
+                  className="form-input"
+                  type="email"
+                  placeholder="Enter email"
+                />
+              </Form.Group>
 
-//               <Form.Group className="mb-3 margin" controlId="formBasicPassword">
-//                 <Form.Control
-//                   className="form-input"
-//                   type="password"
-//                   placeholder="Password"
-//                 />
-//               </Form.Group>
+              <Form.Group className="mb-3 margin" controlId="formBasicPassword">
+                <Form.Control
+                  className="form-input"
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Group>
 
-//               <div className="register_button">
-//                 <Button className="Button" variant="primary" type="submit">
-//                   Login
-//                 </Button>
-//               </div>
-//             </Form>
-//           </div>
-//           <div className="reg_icons">
-//             <div className="icon">
-//               <FaGoogle onClick={() => signInWithGoogle()} />
-//             </div>
-//             <div className="icon">
-//               <FaFacebook />
-//             </div>
-//             <div className="icon">
-//               <FaGithub />
-//             </div>
-//           </div>
-//           <div className="account">
-//             <small>
-//               {" "}
-//               Don't have an Account?
-//               <span>
-//                 {" "}
-//                 <Link to="/register">Register</Link>
-//               </span>
-//             </small>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+              <div className="register_button">
+                <Button className="Button" variant="primary" type="submit">
+                  Login
+                </Button>
+              </div>
+            </Form>
+          </div>
+          <div className="reg_icons">
+            <div className="icon">
+              <FaGoogle onClick={() => signInWithGoogle()} />
+            </div>
+            <div className="icon">
+              <FaFacebook />
+            </div>
+            <div className="icon">
+              <FaGithub />
+            </div>
+          </div>
+          <div className="account">
+            <small>
+              {" "}
+              Don't have an Account?
+              <span>
+                {" "}
+                <Link to="/register">Register</Link>
+              </span>
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default LogIn;
+export default LogIn;
--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -1,76 +1,76 @@
-// import Button from "react-bootstrap/Button";
-// import React from "react";
-// import { Form } from "react-bootstrap";
-// import { Link } from "react-router-dom";
-// import "../AllCss/AllCss.css";
-// import { FaGoogle, FaFacebook,FaGithub } from 'react-icons/fa';
-// const Register = () => {
-//   return (
-//     <div>
-//       <div className="main_registered_part">
-//         <div className="register_area">
-//           <h1 className="reg_heading"> Sign Up </h1>
+import Button from "react-bootstrap/Button";
+import React from "react";
+import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../AllCss/AllCss.css";
+import { FaGoogle, FaFacebook,FaGithub } from 'react-icons/fa';
+const Register = () => {
+  return (
+    <div>
+      <div className="main_registered_part">
+        <div className="register_area">
+          <h1 className="reg_heading"> Sign Up </h1>
 
-//           <div className="register">
-//             <Form>
-//               <Form.Group className="mb-3 margin" controlId="formBasicEmail">
-//                 <Form.Control
-//                   className="form-input"
-//                   type="name"
-//                   placeholder="User Name"
-//                 />
-//               </Form.Group>
+          <div className="register">
+            <Form>
+              <Form.Group className="mb-3 margin" controlId="formBasicEmail">
+                <Form.Control
+                  className="form-input"
+                  type="name"
+                  placeholder="User Name"
+                />
+              </Form.Group>
 
-//               <Form.Group className="mb-3 margin" controlId="formBasicEmail">
-//                 <Form.Control
-//                   className="form-input"
-//                   type="email"
-//                   placeholder="Enter email"
-//                 />
-//               </Form.Group>
+              <Form.Group className="mb-3 margin" controlId="formBasicEmail">
+                <Form.Control
+                  className="form-input"
+                  type="email"
+                  placeholder="Enter email"
+                />
+              </Form.Group>
 
-//               <Form.Group className="mb-3 margin" controlId="formBasicPassword">
-//                 <Form.Control
-//                   className="form-input"
-//                   type="password"
-//                   placeholder="Password"
-//                 />
-//               </Form.Group>
+              <Form.Group className="mb-3 margin" controlId="formBasicPassword">
+                <Form.Control
+                  className="form-input"
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Group>
 
-//               <Form.Group className="mb-3 margin" controlId="formBasicPassword">
-//                 <Form.Control
-//                   className="form-input"
-//                   type="Confirm-password"
-//                   placeholder="Confirm password"
-//                 />
-//               </Form.Group>
+              <Form.Group className="mb-3 margin" controlId="formBasicPassword">
+                <Form.Control
+                  className="form-input"
+                  type="Confirm-password"
+                  placeholder="Confirm password"
+                />
+              </Form.Group>
 
-//               <div className="register_button">
-//                 <Button className="Button" variant="primary" type="submit">
-//                   SignUp
-//                 </Button>
-//               </div>
-//             </Form>
-//           </div>
-//           <div className="reg_icons">
-//           <div className="icon"><FaGoogle /></div>
-//           <div className="icon"><FaFacebook /></div>
-//           <div className="icon"><FaGithub /></div>
-//           </div>
-//           <div className="account">
-//             <small>
-//               {" "}
-//               Don't have an Account?
-//               <span>
-//                 {" "}
-//                 <Link to="/login">Login</Link>
-//               </span>
-//             </small>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+              <div className="register_button">
+                <Button className="Button" variant="primary" type="submit">
+                  SignUp
+                </Button>
+              </div>
+            </Form>
+          </div>
+          <div className="reg_icons">
+          <div className="icon"><FaGoogle /></div>
+          <div className="icon"><FaFacebook /></div>
+          <div className="icon"><FaGithub /></div>
+          </div>
+          <div className="account">
+            <small>
+              {" "}
+              Don't have an Account?
+              <span>
+                {" "}
+                <Link to="/login">Login</Link>
+              </span>
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default Register;
+export default Register;
